1e29afe5bee5b2492c80a99b1113110549d30f2a
AdamHGDD <ash5572@rit.edu>
2020-09-13 23:10:29 -0400

    added comments and finished

--- src/ajaxResponses.js
+++ src/ajaxResponses.js
@@ -1,3 +1,4 @@
+// General method used by any default response
 const respond = (request, response, content, type, status) => {
   response.writeHead(status, { 'Content-Type': type });
   response.write(content);
@@ -6,11 +7,14 @@ const respond = (request, response, content, type, status) => {
 
 
 
+// Helper method for successes
 const getSuccess = (request, response, acceptedTypes) => {
+  // Response object with message
   const jsonReturn = {
     message: 'This is a successful response'
   };
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>${jsonReturn.message}</message>`;
@@ -19,20 +23,25 @@ const getSuccess = (request, response, acceptedTypes) => {
     return respond(request, response, responseXML, 'text/xml', 200);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 200);
 };
 
+// Helper method for bad requests (this one uses params)
 const getBad = (request, response, acceptedTypes, params) => {
+  // Default response object with only a message
   const jsonReturn = {
     message: 'This response has the required parameters',
   };
 
+  // Check paramsto see if object needs it's information switched out
   if (!params.valid || params.valid !== 'true') {
     jsonReturn.id = 'badRequest';
     jsonReturn.message = 'Missing valid query parameter set to true';
   }
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     if(jsonReturn.id)
@@ -45,20 +54,25 @@ const getBad = (request, response, acceptedTypes, params) => {
     return respond(request, response, responseXML, 'text/xml', 400);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 400);
 };
 
+// Helper method for unauthorized access (this one uses params)
 const getUnauthorized = (request, response, acceptedTypes, params) => {
+  // Default response object with only a message
   const jsonReturn = {
     message: 'You have successfully viewed the content',
   };
 
+  // Check paramsto see if object needs it's information switched out
   if (!params.loggedIn || params.loggedIn !== 'yes') {
     jsonReturn.id = 'unauthorized';
     jsonReturn.message = 'Missing loggedIn query parameter set to yes';
   }
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     if(jsonReturn.id)
@@ -71,16 +85,20 @@ const getUnauthorized = (request, response, acceptedTypes, params) => {
     return respond(request, response, responseXML, 'text/xml', 401);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 401);
 };
 
+// Helper method for forbidden access
 const getForbidden = (request, response, acceptedTypes) => {
+  // Response object with message and id
   const jsonReturn = {
     id: 'forbidden',
     message: 'You do not have access to this content',
   };
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     responseXML = `${responseXML} <id>${jsonReturn.id}</id>`;
@@ -90,16 +108,20 @@ const getForbidden = (request, response, acceptedTypes) => {
     return respond(request, response, responseXML, 'text/xml', 403);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 403);
 };
 
+// Helper method for internal server errors
 const getInternalError = (request, response, acceptedTypes) => {
+  // Response object with message and id
   const jsonReturn = {
     id: 'internalError',
     message: 'Internal Server Error. Something went wrong.',
   };
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     responseXML = `${responseXML} <id>${jsonReturn.id}</id>`;
@@ -109,16 +131,20 @@ const getInternalError = (request, response, acceptedTypes) => {
     return respond(request, response, responseXML, 'text/xml', 500);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 500);
 };
 
+// Helper method for not implemented
 const getNotImplemented = (request, response, acceptedTypes) => {
+  // Response object with message and id
   const jsonReturn = {
     id: 'notImplemented',
     message: 'A get request for this page has not been implemented yet. Check again later for updated content.',
   };
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     responseXML = `${responseXML} <id>${jsonReturn.id}</id>`;
@@ -128,16 +154,20 @@ const getNotImplemented = (request, response, acceptedTypes) => {
     return respond(request, response, responseXML, 'text/xml', 501);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 501);
 };
 
+// Helper method for if the page is not found
 const getNotFound = (request, response, acceptedTypes) => {
+  // Response object with message and id
   const jsonReturn = {
     id: 'notFound',
     message: 'The page you are looking for was not found',
   };
 
+  // XML object early return
   if (acceptedTypes[0] === 'text/xml') {
     let responseXML = '<response>';
     responseXML = `${responseXML} <id>${jsonReturn.id}</id>`;
@@ -147,10 +177,14 @@ const getNotFound = (request, response, acceptedTypes) => {
     return respond(request, response, responseXML, 'text/xml', 404);
   }
 
+  // JSON object return
   const jsonString = JSON.stringify(jsonReturn);
   return respond(request, response, jsonString, 'application/json', 404);
 };
 
+
+
+// All exports for server.js to use
 module.exports.getSuccess = getSuccess;
 module.exports.getBad = getBad;
 module.exports.getUnauthorized = getUnauthorized;
